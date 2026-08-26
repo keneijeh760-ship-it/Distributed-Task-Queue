0a079ed8074c1bab8a1a1ad879fc1f3b714dc8c9
Kene Ijeh <keneijeh760@gmail.com>
2026-08-27 00:15:41 +0100

    duplicate tasks tests as well as empty queue tests

---
+++ queue_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"testing"
+)
+
+func TestAddTask_Success(t *testing.T) {
+	q := setupTestQueue(t)
+
+	err := q.AddTask("1", "Task 1 payload")
+	if err != nil {
+		t.Fatalf("failed to add task: %v", err)
+	}
+}
+
+func TestAddTask_DuplicateID(t *testing.T) {
+	q := setupTestQueue(t)
+
+	err := q.AddTask("1", "Task 1 payload")
+	if err != nil {
+		t.Fatalf("failed to add task: %v", err)
+	}
+
+	err = q.AddTask("1", "Task 1 payload")
+	if err == nil {
+		t.Fatal("expected error for duplicate task ID, got nil")
+	}
+
+}
+
+func TestDequeueTask_FIFO(t *testing.T) {
+	q := setupTestQueue(t)
+
+	q.AddTask("1", "Task 1 payload")
+
+	task, err := q.DequeueTask()
+	if err != nil {
+		t.Fatalf("failed to dequeue task: %v", err)
+	}
+	if task.ID != "1" || task.Payload != "Task 1 payload" || task.Status != StatusInProgress {
+		t.Fatal("unexpected task")
+	}
+}
+
+func TestDequeueTask_EmptyQueue(t *testing.T) {
+	q := setupTestQueue(t)
+
+	_, err := q.DequeueTask()
+	if err == nil {
+		t.Fatal("expected error for empty queue, got nil")
+	}
+}
+
+func TestAcknowledge_Success(t *testing.T) {
+	q := setupTestQueue(t)
+
+	q.AddTask("1", "Task 1 payload")
+	task, err := q.DequeueTask()
+	if err != nil {
+		t.Fatalf("failed to dequeue task: %v", err)
+	}
+
+	err = q.Acknowledge(task.ID)
+	if err != nil {
+		t.Fatalf("failed to acknowledge task: %v", err)
+	}
+}
+
+func setupTestQueue(t *testing.T) *Queue {
+	conn, err := connectDB()
+	if err != nil {
+		t.Fatalf("failed to connect: %v", err)
+	}
+	_, err = conn.Exec(context.Background(), "DELETE FROM tasks")
+	if err != nil {
+		t.Fatalf("failed to clean table: %v", err)
+	}
+	return NewQueue(conn)
+}
